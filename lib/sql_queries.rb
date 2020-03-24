b70e14eff4d7c282f9e7a7621922c2f0b0caa70f
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-24 07:59:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS over_goal
   FROM projects
   INNER JOIN pledges ON projects.id = pledges.project_id
-  GROUP BY projects.title;"
+  GROUP BY projects.title;
+  HAVING over_goal >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
